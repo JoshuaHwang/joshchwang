4135ca6d70a225e64877c8b9627781dcf58e17ab
Joshua Hwang <joshchwang@gmail.com>
2016-03-08 22:19:35 -0800

    (#1) added boilerplate code for jade/scss

--- app/app.js
+++ app/app.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var app     = express();
 
+app.set('view engine', 'jade');
+
 app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  res.render('../template/jade/index.jade');
+});
 
 var port = process.env.PORT || 1337;
 
