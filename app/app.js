f7853b9d7e55b6ffb3053a9c8bcb2a36a1240a5f
Joshua Hwang <joshchwang@gmail.com>
2016-03-08 23:35:37 -0800

    (#1) added header logo/links, may change logo

--- app/app.js
+++ app/app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app     = express();
+var path    = require('path');
 
 app.set('view engine', 'jade');
 
-app.use(express.static('public'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
   res.render('../template/jade/index.jade');
