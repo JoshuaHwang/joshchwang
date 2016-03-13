93d954e2315453ca5d0e20d8fb2e013b178b371f
Joshua Hwang <joshchwang@gmail.com>
2016-03-12 16:28:22 -0800

    (#2) made nav fixed to cover whole page

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -3,6 +3,15 @@ var navButton = document.getElementById('hamburger');
 
 navButton.addEventListener('click', function(e) {
   e.preventDefault();
-  console.log('click');
   (this.classList.contains('is-active') === true) ? this.classList.remove('is-active') : this.classList.add('is-active');
+
+  var nav = document.getElementById('nav');
+
+  // if(nav.style.display == 'none') {
+    nav.classList.add('show');
+    nav.classList.remove('hide');
+  // } else {
+    // nav.classList.add('hide');
+    // nav.classList.remove('show');
+  // }
 });
