3db3467f8c0ab2dc243bf456f9c653c2b74b8d99
Joshua Hwang <joshchwang@gmail.com>
2016-03-14 23:41:46 -0700

    (#2) nav open/hide function

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,15 +1,14 @@
 //-- NAV TOGGLE
 var navButton = document.getElementById('hamburger');
 
-navButton.addEventListener('click', function(e) {
-  e.preventDefault();
+$('#hamburger').click(function() {
+  // e.preventDefault();
   (this.classList.contains('is-active') === true) ? this.classList.remove('is-active') : this.classList.add('is-active');
 
-  var nav = document.getElementById('nav');
+  $('#nav').toggle();
+  // var nav = document.getElementById('nav');
 
   // if(nav.style.display == 'none') {
-    nav.classList.add('show');
-    nav.classList.remove('hide');
   // } else {
     // nav.classList.add('hide');
     // nav.classList.remove('show');
