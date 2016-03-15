b454fe000aa7c1075d298c3685d0ceb7e3d93a09
Joshua Hwang <joshchwang@gmail.com>
2016-03-15 00:57:14 -0700

    (#3) hook content added

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,16 +1,7 @@
 //-- NAV TOGGLE
-var navButton = document.getElementById('hamburger');
-
-$('#hamburger').click(function() {
-  // e.preventDefault();
+$('.hamburger').click(function(e) {
+  e.preventDefault();
   (this.classList.contains('is-active') === true) ? this.classList.remove('is-active') : this.classList.add('is-active');
 
-  $('#nav').fadeToggle('fast');
-  // var nav = document.getElementById('nav');
-
-  // if(nav.style.display == 'none') {
-  // } else {
-    // nav.classList.add('hide');
-    // nav.classList.remove('show');
-  // }
+  $('.nav').fadeToggle('fast');
 });
