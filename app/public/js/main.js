47ea01bbaf6fc86ba2c909418c66a8bbdee9c9cb
Joshua Hwang <joshua@Joshuas-MacBook-Pro.local>
2016-04-29 13:08:33 -0700

    Clean slate

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,17 +1,1 @@
-//-- NAV TOGGLE
-$('.hamburger').click(function(e) {
-  e.preventDefault();
-  (this.classList.contains('is-active') === true) ? this.classList.remove('is-active') : this.classList.add('is-active');
 
-  $('.nav').fadeToggle('fast');
-});
-
-//-- SCROLL
-$('.scroll').click(discover);
-$('.about-nav').click(discover);
-
-function discover() {
-  $('html, body').animate({
-    scrollTop: $('.about-row').offset().top
-  }, 1000);
-}
